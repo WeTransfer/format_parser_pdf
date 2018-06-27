d1f2f016b9a3d89e4ab6dead9226a2484d4b1b25
grdw <gerard@wetransfer.com>
2018-06-27 09:55:46 +0200

    Remove useless default specs

--- spec/format_parser_pdf_spec.rb
+++ spec/format_parser_pdf_spec.rb
@@ -1,9 +1,2 @@
 RSpec.describe FormatParserPdf do
-  it "has a version number" do
-    expect(FormatParserPdf::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
