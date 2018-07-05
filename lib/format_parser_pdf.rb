617b559dc05b87df4e74aaeeebd690a9dd76f62e
grdw <gerard@wetransfer.com>
2018-07-05 16:07:43 +0200

    Add IOExtension to add IO methods for PDF::Reader

--- lib/format_parser_pdf.rb
+++ lib/format_parser_pdf.rb
@@ -1,6 +1,6 @@
 require 'pdf-reader'
-require_relative 'pdf_reader_overrides'
 require 'format_parser'
+require 'format_parser_pdf/io_extension'
 require 'format_parser_pdf/version'
 
 module FormatParserPdf
@@ -10,17 +10,19 @@ module FormatParserPdf
     PDF_MARKER = /%PDF-1\.[0-9]{1}/
 
     def call(io)
-      io_constraint = FormatParser::IOConstraint.new(io)
+      io = FormatParser::IOConstraint.new(io)
 
-      return unless safe_read(io_constraint, 9) =~ PDF_MARKER
+      return unless safe_read(io, 9) =~ PDF_MARKER
 
       begin
-        pdf_reader = PDF::Reader.new(io)
+        pdf_reader = PDF::Reader.new(IOExtension.new(io))
 
-        FormatParser::Document.new(
+        doc = FormatParser::Document.new(
           format: :pdf,
           page_count: pdf_reader.page_count
         )
+
+        doc
       rescue PDF::Reader::MalformedPDFError
       end
     end
